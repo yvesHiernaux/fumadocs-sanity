710b36ad80df98078b2080f957d0e9b4de9ca211
Fuma Nama <god63820869@gmail.com>
2024-12-05 22:51:41 +0800

    use vercel url

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -3,10 +3,14 @@ import "@/sanity/types";
 
 import { apiVersion, dataset, projectId } from "../env";
 
+const base = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-  stega: { studioUrl: "http://localhost:3000/studio" },
+  useCdn: true,
+  stega: { studioUrl: `${base}/studio` },
 });
